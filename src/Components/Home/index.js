16b04de80162d052f41730eefd5070b8ce62970d
Yiran Li <yiran.li@hotmail.com>
2021-01-14 20:21:47 -0700

    Add classNames for styling

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { getCatPicture } from '../../apiCalls';
-import './Home.css'
+import './Home.scss'
 
 const Home = () => {
   const [statusCode, setStatusCode] = useState('');
@@ -14,52 +14,58 @@ const Home = () => {
   useEffect(() => generateRandomCatPicture(), [])
   
   return (
-    <section>
-      <section>
-        <div 
-          className="cat-picture-wrapper" 
-          style={{
-            backgroundImage: `url(${pictureUrl})`,
-            backgroundPosition: "center",
-            backgroundSize: "cover",
-            backgroundRepeat: "no-reapeat"
-          }}>
-        </div>
-        <div>
-          <button onClick={generateRandomCatPicture}>What else do you have?</button>
-        </div>
-      </section>
-      <form>
-        <div>
-          <label htmlFor="status-code-input">Status Code</label>
-          <input  
-            id="status-code-input"
-            name="status-code"
-            type="number"
-            value={statusCode}
-            placeholder="i.e 201"
-            onChange={(e) => setStatusCode(e.target.value)}
-          >
-          </input>
-        </div>
-        <div>
-          <label htmlFor="explaination-input">What does {statusCode || 'it'} represents?</label>
-          <input  
-            id="explaination-input"
-            name="status-explaination"
-            type="text"
-            value={explaination}
-            placeholder="i.e Created"
-            onChange={(e) => setExplaination(e.target.value)}
-          >
-          </input>
-        </div>
-        <div>
-          <button>Purrfect! Let's see it!</button>
-        </div>
-      </form>
-      <section>
-        <img />
+    <section className="home-page">
+      <nav className="nav-section">
+        <h1>Purrfect Status</h1>
+        <button>Status Pawtrait</button>
+      </nav>
+      <section className="display-picture-form-section">
+        <section className="picture-section">
+          <div 
+            className="cat-picture-wrapper" 
+            style={{
+              backgroundImage: `url(${pictureUrl})`,
+              backgroundPosition: "center",
+              backgroundSize: "cover",
+              backgroundRepeat: "no-reapeat"
+            }}>
+              <p>{statusCode}</p>
+          </div>
+          <div>
+            <button onClick={generateRandomCatPicture}>Not Purrfect</button>
+          </div>
+        </section>
+        <form className="form-section">
+          <div className="input-wrapper">
+            <label htmlFor="status-code-input">Status Code</label>
+            <input  
+              id="status-code-input"
+              name="status-code"
+              type="number"
+              className="status-input status-code"
+              value={statusCode}
+              placeholder="i.e 201"
+              onChange={(e) => setStatusCode(e.target.value)}
+            >
+            </input>
+          </div>
+          <div className="input-wrapper explaination-input-wrapper">
+            <label htmlFor="explaination-input">What does {statusCode || 'it'} represents?</label>
+            <textarea 
+              id="explaination-input"
+              name="status-explaination"
+              type="text"
+              className="status-input status-explaination"
+              value={explaination}
+              placeholder="i.e Created"
+              onChange={(e) => setExplaination(e.target.value)}
+            >
+            </textarea>
+          </div>
+          <div>
+            <button>Purrfect!</button>
+          </div>
+        </form>
       </section>
     </section>
   )
