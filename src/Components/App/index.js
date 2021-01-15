daddb60cbb051b92304a442216d5d6ba49acde9a
Yiran Li <yiran.li@hotmail.com>
2021-01-14 20:22:00 -0700

    Update className and section for styling

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -1,11 +1,11 @@
-import './App.css';
+import './App.scss';
 import Home from '../Home';
 
 function App() {
   return (
-    <div className="App">
+    <main className="main-body">
       <Home />
-    </div>
+    </main>
   );
 }
 
