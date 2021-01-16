1a2c82f23942b7e199efb932cb543cdd5b36c718
Yiran Li <yiran.li@hotmail.com>
2021-01-16 16:30:18 -0700

    Add more mock data

---
+++ src/Test/_mockData.js
@@ -0,0 +1,10 @@
+export const _mockPicture1 = [{"breeds":[],"id":"weo7Mo87q","url":"https://cdn2.thecatapi.com/images/weo7Mo87q.jpg","width":1124,"height":1500}]
+
+export const _mockPicture2 = [{"breeds":[],"id":"a9l","url":"https://cdn2.thecatapi.com/images/a9l.jpg","width":320,"height":240}]
+
+export const _mockUserInput = {
+  statusCode: '200',
+  explaination: 'Okay',
+  isFavorite: false,
+  id: 123456
+}
