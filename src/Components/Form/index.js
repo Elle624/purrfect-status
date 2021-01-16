1a243d2f167dcbaba95cc60f6ba6f299494e0a1a
Yiran Li <yiran.li@hotmail.com>
2021-01-15 21:38:29 -0700

    Fix indentation and add hover to button

--- src/Components/Form/index.js
+++ src/Components/Form/index.js
@@ -19,36 +19,36 @@ const Form = ({getUserInput}) => {
 
   return (
     <form className="form-section">
-    <div className="input-wrapper">
-      <label htmlFor="status-code-input">Status Code</label>
-      <input  
-        id="status-code-input"
-        name="status-code"
-        type="number"
-        className="status-input status-code"
-        value={statusCode}
-        placeholder="i.e 201"
-        onChange={(e) => setStatusCode(e.target.value)}
-      >
-      </input>
-    </div>
-    <div className="input-wrapper explaination-input-wrapper">
-      <label htmlFor="explaination-input">What does {statusCode || 'it'} represents?</label>
-      <textarea 
-        id="explaination-input"
-        name="status-explaination"
-        type="text"
-        className="status-input status-explaination"
-        value={explaination}
-        placeholder="i.e Created"
-        onChange={(e) => setExplaination(e.target.value)}
-      >
-      </textarea>
-    </div>
-    <div>
-      <button onClick={handleClick}>Purrfect!</button>
-    </div>
-  </form>
+      <div className="input-wrapper">
+        <label htmlFor="status-code-input">Status Code</label>
+        <input  
+          id="status-code-input"
+          name="status-code"
+          type="number"
+          className="status-input status-code"
+          value={statusCode}
+          placeholder="i.e 201"
+          onChange={(e) => setStatusCode(e.target.value)}
+        >
+        </input>
+      </div>
+      <div className="input-wrapper explaination-input-wrapper">
+        <label htmlFor="explaination-input">What does {statusCode || 'it'} represents?</label>
+        <textarea 
+          id="explaination-input"
+          name="status-explaination"
+          type="text"
+          className="status-input status-explaination"
+          value={explaination}
+          placeholder="i.e Created"
+          onChange={(e) => setExplaination(e.target.value)}
+        >
+        </textarea>
+      </div>
+      <div>
+        <button onClick={handleClick}>Purrfect!</button>
+      </div>
+    </form>
   )
 }
 
